1b1484890668dfd386ae0b575938bc7694ce7cb5
Yeasir Arafat <yeasirarafat321@gmail.com>
2019-03-03 02:35:28 +0600

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>ContactStore</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            <div id="app">
+                <Contacts></Contacts>
+            </div>
+        </div>
+    <script src="/js/app.js"></script>
+    </body>
+</html>
